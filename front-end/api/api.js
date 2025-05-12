33c797730c763c60396c0c765045d2403841e528
santosjoao07 <santosgomesjoaopaulo@gmail.com>
2025-05-11 22:30:14 -0300

    adjust in process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-import "dotenv/config";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://spotify-gp57.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
