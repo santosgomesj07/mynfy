4d69597d3a8be44f08f826e6d57d65eccee7aac5
santosjoao07 <santosgomesjoaopaulo@gmail.com>
2025-05-11 21:47:18 -0300

    adjust in deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,18 +1,30 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
+// app.use(express.json());
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+// Serve os arquivos estáticos do front-end
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+// Rota catch-all para aplicações SPA (ex: React, Vue, Angular)
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
